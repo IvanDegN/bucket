5b771392bc29666a2e901e2546dae05e281bdcee
Ivan <mauzer.bruno@yandex.ru>
2025-03-10 23:03:17 +0300

    create click for button

---
+++ script.js
@@ -0,0 +1,9 @@
+let elemnt = document.getElementById('btnAddToBucket'); // получение доступа к кнопке по id
+
+elemnt.addEventListener('click', (event) =>{
+    console.log('click')
+
+
+    
+
+} );
