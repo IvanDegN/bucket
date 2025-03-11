070d1a97ac2546d1220f1a4e92182a22b506a739
Ivan <mauzer.bruno@yandex.ru>
2025-03-11 20:39:13 +0300

    add function to add item in object

--- script.js
+++ script.js
@@ -1,9 +1,27 @@
 let elemnt = document.getElementById('btnAddToBucket'); // получение доступа к кнопке по id
 
-elemnt.addEventListener('click', (event) =>{
-    console.log('click')
+let items = {
+   item0: { id: 1, title: 'яблоко', cost: 45},    
+   item1: {id: 2, title: 'молоко', cost: 43}
+};
 
+elemnt.addEventListener('click', (event) =>{
+    let idInObj
+    for(let el in items){
+
+        idInObj = items[el].id;
+    }
+
+    let title = prompt('enter title')
+    let cost = prompt('eneter cost')
 
     
+    console.log(title)
+    console.log(cost)
 
+    let newItem = {id: idInObj + 1, title: title, cost: cost}
+    items.item2 = newItem;
+
+    console.log(items)
+    
 } );
